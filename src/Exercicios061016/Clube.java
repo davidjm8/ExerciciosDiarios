c4878280a062ea983e94d77a0c73c5524743de13
UserPL022Pc01 <UserPL022Pc01@SLFRPL022PC01.ensino.atec.local>
2016-10-10 08:40:35 +0100

    çºç

--- src/Exercicios061016/Clube.java
+++ src/Exercicios061016/Clube.java
@@ -1,5 +1,57 @@
 package Exercicios061016;
 
 public class Clube {
+	
+	String Nome; 
+	int pontosCasa; 
+	int pontosFora; 
+	int numJogos; 
+	
+	public Clube(){}
+	
+	public Clube(String n){
+		this.Nome = n;
+		
+		
+	}
 
+	public Clube(String nome, int pontosCasa, int pontosFora, int numJogos) {
+		super();
+		Nome = nome;
+		this.pontosCasa = pontosCasa;
+		this.pontosFora = pontosFora;
+		this.numJogos = numJogos;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public int getPontosCasa() {
+		return pontosCasa;
+	}
+
+	public void setPontosCasa(int pontosCasa) {
+		this.pontosCasa = pontosCasa;
+	}
+
+	public int getPontosFora() {
+		return pontosFora;
+	}
+
+	public void setPontosFora(int pontosFora) {
+		this.pontosFora = pontosFora;
+	}
+
+	public int getNumJogos() {
+		return numJogos;
+	}
+
+	public void setNumJogos(int numJogos) {
+		this.numJogos = numJogos;
+	}
 }
--- src/Exercicios071016/Treino.java
+++ src/Exercicios071016/Treino.java
@@ -1,10 +1,11 @@
 package Exercicios071016;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Treino {
 	public Treino(){
-		exercicio1();
+		exercicio2();
 		
 	}
 	
@@ -20,5 +21,40 @@ public class Treino {
 			
 		}
 	}
-
+	
+	private void exercicio2() {
+		System.out.println("Introduza o numero 1");
+		int num1 = (new Scanner(System.in)).nextInt(); 
+		System.out.println("Introduza o numero 2");
+		int num2 = (new Scanner(System.in)).nextInt();
+		System.out.println("Introduza o numero 3");
+		int num3 = (new Scanner(System.in)).nextInt();
+		
+		if (num1 > num2 && num1 > num3) {
+			System.out.println("O numero 1 eh o maior");
+		} else if (num2 > num1 && num2 > num3 )
+				System.out.println("O numero 2 eh o maior");
+		else if (num3 > num1 && num3 > num2)
+			System.out.println("O numero 3 eh o maior");
+			
+		}
+	
+	private void exercicio3() {
+		int limite = 5;
+		int rand = (new Random()).nextInt(5) + 1;
+		
+		for (int i=0; i<5; i++) {
+			System.out.println("introduza a " + (i+1) + "tentativa");
+			int tent = ((new Scanner (System.in)).nextInt());
+			
+			if (tent == rand) {
+			System.out.println("Acertou à "+ (i+1) + "tentativa");
+			return;
+			} else 
+				System.out.println("Vá tentando que estou estou gostando ");
+		}
+		System.out.println("Tens 5 tentativas para acertares num n de 1 a 5 e falhas  ");
+	
+	}
 }
+
